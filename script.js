659c7a6bd398d6053d0a3f39613915e6a22b5ab3
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-04-22 20:39:25 +0530

    Added code for a multi connection setup

--- script.js
+++ script.js
@@ -3,16 +3,23 @@
     let input1 = null;
     let sendButton1 = null;
     let disconnectButton1 = null;
-    let channel1 = null;
-    let peer1 = null;
-    
+    let channel1_2 = null;
+    let peer1_2 = null;
+    let peer1_3 = null;
+
     //peer 2
     let input2 = null;
     let sendButton2 = null;
     let disconnectButton2 = null;
     let channel2 = null;
-    let peer2;
-    
+    let peer2 = null;
+
+    //peer 3
+    let input3 = null;
+    let sendButton3 = null;
+    let disconnectButton3 = null;
+    let channel3 = null;
+    let peer3 = null;
     //message display div
     let messageDiv = null;
 
@@ -21,38 +28,54 @@
         input1 = document.getElementById('inp-1');
         sendButton1 = document.getElementById('btn-1');
         disconnectButton1 = document.getElementById('disconn-1');
-        div1 = document.getElementById('received-message1');
 
         input2 = document.getElementById('inp-2');
         sendButton2 = document.getElementById('btn-2');
         disconnectButton2 = document.getElementById('disconn-2');
-        div2 = document.getElementById('received-message2');
+
+        input3 = document.getElementById('inp-3');
+        sendButton3 = document.getElementById('btn-3');
+        disconnectButton3 = document.getElementById('disconn-3');
 
         messageDiv = document.getElementById('messages');
 
         sendButton1.addEventListener('click', onSend1);
         sendButton2.addEventListener('click', onSend2);
+        sendButton3.addEventListener('click', onSend3);
 
         disconnectButton1.addEventListener('click', onDisconnect1);
         disconnectButton2.addEventListener('click', onDisconnect2);
+        disconnectButton3.addEventListener('click', onDisconnect3);
 
         input1.addEventListener('keyup', (event) => {
-            if(event.key === 'Enter') {
+            if (event.key === 'Enter') {
                 sendButton1.click();
             }
         })
 
         input2.addEventListener('keyup', (event) => {
-            if(event.key === 'Enter') {
+            if (event.key === 'Enter') {
                 sendButton2.click();
             }
         })
 
-        //scenario for two peers using single data channel using out-of-band negotiation using an agreed upon id
-        peer1 = new RTCPeerConnection();
-        channel1 = peer1.createDataChannel("chat", { negotiated: true, id: 1 });
+        input3.addEventListener('keyup', (event) => {
+            if (event.key === 'Enter') {
+                sendButton3.click();
+            }
+        })
 
-        channel1.onmessage = function (event) {
+        //scenario for two peers using single data channel using out-of-band negotiation using an agreed upon id
+        peer1_2 = new RTCPeerConnection();
+        channel1_2 = peer1_2.createDataChannel("chat", { negotiated: true, id: 1 });
+
+        channel1_2.onmessage = function (event) {
+            handleReceivedMessage('Peer 1 received: ' + event.data);
+        }
+
+        peer1_3 = new RTCPeerConnection();
+        channel1_3 = peer1_3.createDataChannel("chat", { negotiated: true, id: 1 });
+        channel1_3.onmessage = function (event) {
             handleReceivedMessage('Peer 1 received: ' + event.data);
         }
 
@@ -62,6 +85,14 @@
 
         channel2.onmessage = function (event) {
             handleReceivedMessage('Peer 2 received: ' + event.data);
+        }
+
+
+        peer3 = new RTCPeerConnection();
+        channel3 = peer3.createDataChannel("chat", { negotiated: true, id: 1 });
+
+        channel3.onmessage = function (event) {
+            handleReceivedMessage('Peer 3 received: ' + event.data);
         }
 
         //Scenario for multiple data channels
@@ -81,24 +112,44 @@
         //     }
         // }
 
-        peer1.onicecandidate = function (event) {
+        peer1_2.onicecandidate = function (event) {
             if (event.candidate) {
                 peer2.addIceCandidate(event.candidate);
             }
         }
 
-        peer2.onicecandidate = function (event) {
-            if (event.candidate) {
-                peer1.addIceCandidate(event.candidate);
+        peer1_3.onicecandidate = function (event) {
+            if(event.candidate) {
+                peer3.addIceCandidate(event.candidate);
             }
         }
 
-        peer1.createOffer()
-            .then(offer => peer1.setLocalDescription(offer))
-            .then(() => peer2.setRemoteDescription(peer1.localDescription))
+        peer2.onicecandidate = function (event) {
+            if (event.candidate) {
+                peer1_2.addIceCandidate(event.candidate);
+            }
+        }
+
+        peer3.onicecandidate = function (event) {
+            if (event.candidate) {
+                peer1_3.addIceCandidate(event.candidate);
+            }
+        }
+
+        peer1_2.createOffer()
+            .then(offer => peer1_2.setLocalDescription(offer))
+            .then(() => peer2.setRemoteDescription(peer1_2.localDescription))
             .then(() => peer2.createAnswer())
             .then(answer => peer2.setLocalDescription(answer))
-            .then(() => peer1.setRemoteDescription(peer2.localDescription))
+            .then(() => peer1_2.setRemoteDescription(peer2.localDescription))
+            .catch(console.log);
+
+        peer1_3.createOffer()
+            .then(offer => peer1_3.setLocalDescription(offer))
+            .then(() => peer3.setRemoteDescription(peer1_3.localDescription))
+            .then(() => peer3.createAnswer())
+            .then(answer => peer3.setLocalDescription(answer))
+            .then(() => peer1_3.setRemoteDescription(peer3.localDescription))
             .catch(console.log);
     }
 
@@ -110,8 +161,11 @@
     }
 
     function onDisconnect1() {
-        channel1.close();
-        peer1.close();
+        channel1_2.close();
+        peer1_2.close();
+
+        channel1_3.close();
+        peer1_3.close();
     }
 
     function onDisconnect2() {
@@ -119,23 +173,44 @@
         peer2.close();
     }
 
+    function onDisconnect3() {
+        channel3.close();
+        peer3.close();
+    }
+
     function onSend1() {
 
-        console.log('channel 1 ready state: ' + channel1.readyState);
-    
-        if (channel1.readyState === "open") {
+        console.log('channel 1_2 ready state: ' + channel1_2.readyState);
+        console.log('channel 1_3 ready state: ' + channel1_3.readyState);
+
+        if (channel1_2.readyState === "open") {
             const message = input1.value;
-            channel1.send(message);
+            channel1_2.send(message);
+        }
+
+        if (channel1_3.readyState === "open") {
+            const message = input1.value;
+            channel1_3.send(message);
         }
     }
 
     function onSend2() {
 
         console.log('channel 2 ready state: ' + channel2.readyState);
-        
+
         if (channel2.readyState === "open") {
             const message = input2.value;
             channel2.send(message);
+        }
+    }
+
+    function onSend3() {
+
+        console.log('channel 3 ready state: ' + channel3.readyState);
+
+        if (channel3.readyState === "open") {
+            const message = input3.value;
+            channel3.send(message);
         }
     }
 
